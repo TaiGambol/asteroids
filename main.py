ea8219c64f7f447ddeeabbc6a66d95a849ec5c75
TaiGambol <TaiJParry@googlemail.com>
2025-07-08 23:31:08 +0100

    first real commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+    print("Starting Asteroids!")
+    print(f"Screen width: 1280")
+    print(f"Screen height: 720")
+
+if __name__ == "__main__":
+    main()
