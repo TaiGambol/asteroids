e47e8eec6502539472529036d911a706a340a0dc
TaiGambol <TaiJParry@googlemail.com>
2025-07-10 23:39:12 +0100

    Finished project, should probably have been doing more commits huh.

---
+++ asteroid.py
@@ -0,0 +1,10 @@
+from circleshape import *
+from constants import *
+
+class Asteroid(CircleShape)
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+        
